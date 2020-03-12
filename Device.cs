ce2029aa905caf7183eabda3f424c8dd27263259
Ruslan Birzhanov <birganovruslan@yandex.ru>
2020-03-12 10:53:02 +0100

    Added switch:case structure to handle different types of events

--- Device.cs
+++ Device.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class Device : MonoBehaviour
 {
     public float DeviceData;
-    public string DeviceLog;
+    public enum EventType {Success, Neutral, Critical}
+    public string SuccessEvent_Log;
+    public string NeutralEvent_Log;
+    public string CriticalEvent_Log;
     public bool HasPlayerWorked;
 
     public GameHandler Manager;
+
+    public EventType eventType;
+    void Start(){
+        eventType = EventType.Neutral;
+    }
+
+    public void RegisterEvent(){
+        
+        switch(eventType){
+            case EventType.Success:
+                break;
+            case EventType.Neutral:
+                break;
+            case EventType.Critical:
+                break;
+
+        }
+    }
 
     public void DeviceInteraction(){
 
